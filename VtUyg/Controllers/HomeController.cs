bf1595b4c55a41abf01f475e27e18428dbd8166e
iremicyer <iremicyer@gmail.com>
2020-09-16 15:12:28 +0300

    Add project files.

---
+++ VtUyg/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Documents;
+using VtUyg.Models;
+using VtUyg.ViewModel;
+
+namespace VtUyg.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        db01Entities db = new db01Entities();
+        public ActionResult Index()
+        {
+            List<Kayitlar> kayitListe = db.Kayitlar.ToList();
+            return View(kayitListe);
+        }
+
+        public ActionResult yeniKayit()
+        {
+            return View();
+        }
+       [HttpPost]
+        public ActionResult yeniKayit(kayitModel model)
+        {
+            Kayitlar kayit = new Kayitlar();
+            kayit.adsoyad = model.adsoyad;
+            kayit.mail = model.mail;
+            kayit.yas = model.yas;
+
+            db.Kayitlar.Add(kayit); //veritabanındaki kayıtlar ekleniyor.
+            db.SaveChanges(); // olası değişiklikleri kaydetmek için.
+
+            ViewBag.sonuc = "Kayıt Yapıldı.";// ViewBag,homecontrol'deki yapıyı view'e taşır
+
+
+            return View();
+        }
+    
+        public ActionResult kayitDuzenle(int? id)
+        {
+
+            Kayitlar kayit = db.Kayitlar.Where(k => k.kayId == id).SingleOrDefault();
+
+            kayitModel model = new kayitModel();
+            model.kayId = kayit.kayId;
+            model.adsoyad = kayit.adsoyad;
+            model.mail = kayit.mail;
+            model.yas = kayit.yas;
+            return View(model);
+        }
+
+        [HttpGet]
+
+        public ActionResult kayitSil(int? id)
+        {
+            Kayitlar kayit = db.Kayitlar.Where(k => k.kayId == id).SingleOrDefault();
+
+            db.Kayitlar.Remove(kayit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult kayitDuzenle(kayitModel m)
+        {
+
+            Kayitlar kayit = db.Kayitlar.Where(k => k.kayId == m.kayId).SingleOrDefault();
+            kayit.adsoyad = m.adsoyad;
+            kayit.mail = m.mail;
+            kayit.yas = m.yas;
+
+            db.SaveChanges(); // yapılan değişiklikleri-güncellemeleri kaydeder.
+
+
+
+            ViewBag.sonuc = "Kayıt Güncellendi";// ViewBag,homecontrol'deki yapıyı view'e taşır
+            return View();
+        }
+    }
+}
---
+++ VtUyg/ViewModel/kayitModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VtUyg.ViewModel
+{
+    public class kayitModel
+    {
+        public int kayId { get; set; }
+
+
+        [Required(ErrorMessage = "Adı Soyadı Giriniz!")]  // model özellikleri için doğrulama kuralları belirtmemize olanak sağlar.
+        [Display(Name = "Adı Soyadı")] // ekran adı ekler
+        [StringLength(500, ErrorMessage = "Adı Soyadı En Fazla 500 Karakter Olmalı!")]
+        public string adsoyad { get; set; }
+
+        [Required(ErrorMessage = "E-Posta Giriniz!")]  // model özellikleri için doğrulama kuralları belirtmemize olanak sağlar.
+        [Display(Name = "E-Posta")] // ekran adı ekler
+        [StringLength(500 ,ErrorMessage = "E-Posta En Fazla 500 Karakter Olmalı!")]
+
+        public string mail { get; set; }
+
+        [Required(ErrorMessage = "Yaş Giriniz!")]  // model özellikleri için doğrulama kuralları belirtmemize olanak sağlar.
+        [Display(Name = "Yaş")] // ekran adı ekler
+
+        public int yas { get; set; }
+    
+    } 
+}
